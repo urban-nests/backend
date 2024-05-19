0482d8b357abe1b2fe3414f801a67878e3fcf3df
given-dragon <yijy001@gmail.com>
2024-05-19 19:18:38 +0900

    feat: 커서기반 list 조회, deal id별 member 정보 조회, deal detail 조회 추가

--- src/main/java/com/urbannest/backend/domain/housedeal/repository/HouseDealRepository.java
+++ src/main/java/com/urbannest/backend/domain/housedeal/repository/HouseDealRepository.java
@@ -1,7 +1,24 @@
 package com.urbannest.backend.domain.housedeal.repository;
 
+import com.urbannest.backend.domain.housedeal.dto.projection.DealIdWithMemberProjection;
+import com.urbannest.backend.domain.housedeal.dto.projection.SimpleHouseDealProjection;
 import com.urbannest.backend.domain.housedeal.entity.HouseDeal;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface HouseDealRepository extends JpaRepository<HouseDeal, Long> {
+
+    @Query("select hd from HouseDeal hd join fetch hd.houseInfo hi where hd.no <= :id")
+    List<SimpleHouseDealProjection> findHouseDealsAfterId(@Param("id") Long id, Pageable pageable);
+
+    @Query("select hd from HouseDeal hd join fetch hd.member m where hd.no in :dealIdList")
+    List<DealIdWithMemberProjection> findMembersByDealIds(@Param("dealIdList") List<Long> dealIdList);
+
+    @Query("select hd from HouseDeal hd join fetch hd.houseInfo join fetch hd.member m where hd.no = :id ")
+    Optional<HouseDeal> findById(@Param("id") Long id);
 }

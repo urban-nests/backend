7d130056c457beee2fa823fbbf1f826d1049f5a1
given-dragon <yijy001@gmail.com>
2024-05-20 02:20:05 +0900

    [urban-nests/plan#16] feat: 조회수 증가 쿼리 추가

--- src/main/java/com/urbannest/backend/domain/housedeal/repository/HouseDealRepository.java
+++ src/main/java/com/urbannest/backend/domain/housedeal/repository/HouseDealRepository.java
@@ -5,6 +5,7 @@ import com.urbannest.backend.domain.housedeal.dto.projection.SimpleHouseDealProj
 import com.urbannest.backend.domain.housedeal.entity.HouseDeal;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -21,4 +22,9 @@ public interface HouseDealRepository extends JpaRepository<HouseDeal, Long> {
 
     @Query("select hd from HouseDeal hd join fetch hd.houseInfo hi join fetch hd.member m join fetch hi.dongCode where hd.no = :id ")
     Optional<HouseDeal> findById(@Param("id") Long id);
+
+    @Modifying
+    @Query("update HouseDeal hd set hd.hit=hd.hit+1 where hd.no=:id")
+    void incrementHit(@Param("id") Long id);
+
 }
